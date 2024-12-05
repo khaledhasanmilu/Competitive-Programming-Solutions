04433ebecc3f588ed2ecb44d98ca44308bfee7e1
Khaled Hasan Milu <mmilu223104@bscse.uiu.ac.bd>
2024-12-05 16:36:13 +0600

    change  repo

--- Hackerrank/sgjjjj.cpp
+++ Hackerrank/sgjjjj.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 int main()
 {
+   cout<<"This is khaled";
+   int a,b;
+   if (a<b)
+   {
+    cout<<"hum";
+    cout<<"h";
+   }
+   else
+   {
+    cout<<"No"<<endl;
+   }
+   
    
  return 0;
 }
